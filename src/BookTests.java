84ae0bf16a89dfa663d42b9889d27b61414f37dd
Dina Migunova <migunova.dina@gmail.com>
2023-03-24 10:56:06 +0100

    Add BookWithNoAuthor(), BookWithNoTitle()

--- src/BookTests.java
+++ src/BookTests.java
@@ -1,6 +1,33 @@
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 public class BookTests {
+
+    @Test
+    public void BookWithNoAuthor() {
+        boolean exceptionThrown = false;
+        try {
+            Book b = new Book("", "A");
+        } catch (IllegalArgumentException e) {
+            exceptionThrown = true;
+        }
+
+        assertTrue(exceptionThrown);
+    }
+
+    @Test
+    public void BookWithNoTitle() {
+        boolean exceptionThrown = false;
+        try {
+            Book b = new Book("A", "");
+        } catch (IllegalArgumentException e) {
+            exceptionThrown = true;
+        }
+
+        assertTrue(exceptionThrown);
+    }
+
+
 }
