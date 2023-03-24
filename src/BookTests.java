1984596a79f055262d04ff099cef0741e8744a92
Dina Migunova <migunova.dina@gmail.com>
2023-03-24 11:12:26 +0100

    Add Test BookAuthorComparator()

--- src/BookTests.java
+++ src/BookTests.java
@@ -3,6 +3,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookTests {
 
     @Test
@@ -27,6 +31,27 @@ public class BookTests {
         }
 
         assertTrue(exceptionThrown);
+    }
+
+    @Test
+    public void BookAuthorComparator() {
+        Book b1 = new Book("A", "B");
+        Book b2 = new Book("B", "Al");
+        Book b3 = new Book("A", "Yu");
+
+        List<Book> actual = new ArrayList<>();
+        actual.add(b1);
+        actual.add(b2);
+        actual.add(b3);
+
+        List<Book> expected = new ArrayList<>();
+        expected.add(b1);
+        expected.add(b3);
+        expected.add(b2);
+
+        actual.sort(new BookAuthorComparator());
+
+        assertEquals(expected, actual);
     }
 
 
